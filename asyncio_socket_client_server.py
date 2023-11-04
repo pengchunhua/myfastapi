0cbc5a651acb5f4026d170641f4b4517d253512d
pengchunhua <38714669+pengchunhua@users.noreply.github.com>
2023-11-04 09:13:19 +0800

    更新异步socket的服务端和客户端

--- asyncio_socket_client_server.py
+++ asyncio_socket_client_server.py
@@ -1,3 +1,28 @@
+# ----------------------------------客户端---------------------------------------------
+import asyncio
+import socket
+sock = socket.socket()
+sock.send()
+async def tcp_echo_client(message):
+    reader, writer = await asyncio.open_connection(
+        '127.0.0.1', 8888)
+
+    print(reader, id(reader), writer, id(writer))
+    print(f'Send: {message}')
+    writer.write(message.encode())
+    await writer.drain()
+
+    data = await reader.read(100)
+    print(f'Received: {data.decode()}')
+
+    print('Close the connection')
+    writer.close()
+    await writer.wait_closed()
+
+asyncio.run(tcp_echo_client('Hello World!'))
+
+
+# ----------------------------------------服务端-----------------------------------------
 class AsyncPowerServer(object):
     def __init__(self):
         self.ip = Settings().POWER_IP
