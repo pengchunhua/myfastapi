601a87c9536685ad72623be7365276e3c8ae0317
pengchunhua <38714669+pengchunhua@users.noreply.github.com>
2023-11-06 16:10:35 +0800

    防死锁问题(哲学家就餐问题)

---
+++ threading_lock/protect_dead_lock.py
@@ -0,0 +1,99 @@
+import threading
+from contextlib import contextmanager
+
+# 参考文档：https://python3-cookbook.readthedocs.io/zh-cn/latest/c12/p05_locking_with_deadlock_avoidance.html
+# Thread-local state to stored information on locks already acquired
+_local = threading.local()
+
+@contextmanager
+def acquire(*locks):
+    # Sort locks by object identifier
+    locks = sorted(locks, key=lambda x: id(x))
+
+    # Make sure lock order of previously acquired locks is not violated
+    acquired = getattr(_local,'acquired',[])
+    if acquired and max(id(lock) for lock in acquired) >= id(locks[0]):
+        raise RuntimeError('Lock Order Violation')
+
+    # Acquire all of the locks
+    acquired.extend(locks)
+    _local.acquired = acquired
+
+    try:
+        for lock in locks:
+            lock.acquire()
+        yield
+    finally:
+        # Release locks in reverse order of acquisition
+        for lock in reversed(locks):
+            lock.release()
+        del acquired[-len(locks):]
+
+# 使用者，避免死锁的原因是对锁按照id进行了排序
+import threading
+x_lock = threading.Lock()
+y_lock = threading.Lock()
+
+def thread_1():
+    while True:
+        with acquire(x_lock, y_lock):
+            print('Thread-1')
+
+def thread_2():
+    while True:
+        with acquire(y_lock, x_lock):
+            print('Thread-2')
+
+t1 = threading.Thread(target=thread_1)
+t1.daemon = True
+t1.start()
+
+t2 = threading.Thread(target=thread_2)
+t2.daemon = True
+t2.start()
+
+# 这种嵌套锁问题不能使用上述方案处理
+import threading
+x_lock = threading.Lock()
+y_lock = threading.Lock()
+
+def thread_1():
+
+    while True:
+        with acquire(x_lock):
+            with acquire(y_lock):
+                print('Thread-1')
+
+def thread_2():
+    while True:
+        with acquire(y_lock):
+            with acquire(x_lock):
+                print('Thread-2')
+
+t1 = threading.Thread(target=thread_1)
+t1.daemon = True
+t1.start()
+
+t2 = threading.Thread(target=thread_2)
+t2.daemon = True
+t2.start()
+
+
+# 哲学家就餐问题
+import threading
+
+# The philosopher thread
+def philosopher(left, right):
+    while True:
+        with acquire(left,right):
+             print(threading.currentThread(), 'eating')
+
+# The chopsticks (represented by locks)
+NSTICKS = 5
+chopsticks = [threading.Lock() for n in range(NSTICKS)]
+
+# Create all of the philosophers
+for n in range(NSTICKS):
+    t = threading.Thread(target=philosopher,
+                         args=(chopsticks[n],chopsticks[(n+1) % NSTICKS]))
+    t.start()
