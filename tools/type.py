a1076435cf7c40353ce7377d1ccb00f06d33d62a
pengchunhua <38714669+pengchunhua@users.noreply.github.com>
2023-11-07 14:46:37 +0800

    更新type.py

--- tools/type.py
+++ tools/type.py
@@ -21,3 +21,29 @@ import types
 
 Stock = types.new_class('Stock', (), {}, lambda ns: ns.update(cls_dict))
 Stock.__module__ = __name__
+
+
+import operator
+import types
+import sys
+
+def named_tuple(classname, fieldnames):
+    # Populate a dictionary of field property accessors
+    cls_dict = { name: property(operator.itemgetter(n))
+                for n, name in enumerate(fieldnames) }
+
+    # Make a __new__ function and add to the class dict
+    def __new__(cls, *args):
+        if len(args) != len(fieldnames):
+            raise TypeError('Expected {} arguments'.format(len(fieldnames)))
+        return tuple.__new__(cls, args)
+
+    cls_dict['__new__'] = __new__
+
+    # Make the class
+    cls = types.new_class(classname, (tuple,), {},
+                        lambda ns: ns.update(cls_dict))
+
+    # Set the module to that of the caller
+    cls.__module__ = sys._getframe(1).f_globals['__name__']
+    return cls
