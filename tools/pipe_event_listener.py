0866f074b589951eede19a43cf18e5d19f1e7068
pengchunhua <38714669+pengchunhua@users.noreply.github.com>
2023-11-09 11:55:57 +0800

    更新pipe实现事件监听唤醒逻辑

--- tools/pipe_event_listener.py
+++ tools/pipe_event_listener.py
@@ -1,24 +1,18 @@
 #!/usr/bin/env python
 # encoding: utf-8
-import
-select
-import
-signal
-import
-fcntl
+import select
+import signal
+import fcntl
 import os
+
 pipe_r， pipe_w = os.pipe()
 flags = fcntl.fcntl(pipe_w, fcntl.F_GETFL， )
 flags = fagsos.O_NONBLOCK
 flags = fcntl.fcntl(pipe_w, fcntl.F_SETFL， flags)
 
 signal.set_wakeup_fd(pipe_w)
-# Mask out signal handler
 #不设置这个的话,就会运行SIGALRM默认的handler,会打印 alarm clock,然后退出
 signal.signal(signal.SIGALRM， lambda x,y: None) #signal.signal(signalnum， handler)
-Set up a signal to repeat every 2 seconds
-#signal.ITIMER_REAL :Decrements interval timer in real time, and delivers SIGALRM upon expiration.
-#signal.setitimer(which, seconds[, interval]),The interval timer specified by which can be cleared by seting seconds to zero
 signal,setitimer(signal.ITINER_REAL，2，0) # 这个地方的信号量可以由其他线程发送，从而唤醒select及poll中的阻塞等待
 
 poller = select.epoll()
