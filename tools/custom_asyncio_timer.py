fde6a401a55792747305151e00e68b7d218315b8
pengchunhua <38714669+pengchunhua@users.noreply.github.com>
2023-11-24 11:21:00 +0800

    使用asyncio实现自定义定时器

--- tools/custom_asyncio_timer.py
+++ tools/custom_asyncio_timer.py
@@ -14,10 +14,10 @@ def complete(loop, future):
     loop.stop()
 
 def kk():
-    task = asyncio.create_task(print_hello())
-    task.add_done_callback(partial(complete, loop))
+    task = asyncio.create_task(print_hello())  # 将协程封装为task任务
+    task.add_done_callback(partial(complete, loop))  # 任务完成后的回调函数，注意该函数的参数必须带future,如果需要其他参数可以使用partial来设置默认值
 
 if __name__ == "__main__":
     loop = asyncio.get_event_loop()
-    loop.call_later(10, kk)
+    loop.call_later(10, kk)  # 将对应的task任务封装成Handle、TimerHandle，之后调度器使用hanle中的run方法调用执行对应的任务
     loop.run_forever()
