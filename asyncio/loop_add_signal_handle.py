ba6a910a352b07f1bbb212b7d4deaf678ed511c3
pengchunhua <38714669+pengchunhua@users.noreply.github.com>
2023-12-01 08:35:10 +0800

    异步协程在主线程中增加signal处理器

---
+++ asyncio/loop_add_signal_handle.py
@@ -0,0 +1,21 @@
+  def install_signal_handlers(self) -> None:
+      if threading.current_thread() is not threading.main_thread():
+          # Signals can only be listened to from the main thread.
+          return
+
+      loop = asyncio.get_event_loop()
+
+      try:
+          for sig in HANDLED_SIGNALS:
+              loop.add_signal_handler(sig, self.handle_exit, sig, None)
+      except NotImplementedError:  # pragma: no cover
+          # Windows
+          for sig in HANDLED_SIGNALS:
+              signal.signal(sig, self.handle_exit)
+
+  def handle_exit(self, sig: signal.Signals, frame: FrameType) -> None:
+
+      if self.should_exit and sig == signal.SIGINT:
+          self.force_exit = True
+      else:
+          self.should_exit = True
