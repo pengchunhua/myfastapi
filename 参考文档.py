ef8dd02ff3af79b351755cdaa2449638c6fe58de
pengchunhua <38714669+pengchunhua@users.noreply.github.com>
2023-12-08 15:05:29 +0800

    Update 参考文档.py

--- 参考文档.py
+++ 参考文档.py
@@ -92,6 +92,8 @@ python使用文档：
   37、asyncio实现协程池：
     1、https://blog.csdn.net/weixin_43968923/article/details/111397237
     2、https://blog.csdn.net/weixin_45063926/article/details/130450167
+  38、python实现流媒体控制：
+    1、https://blog.csdn.net/qq_47452807/article/details/124219048
 
 java中的参考文档：
   netty能做什么：https://www.zhihu.com/question/24322387
