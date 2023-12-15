16e5787daa00cbe3fbcf433e121de8f8a80f6490
pengchunhua <38714669+pengchunhua@users.noreply.github.com>
2023-12-15 09:50:11 +0800

    Update 参考文档.py

--- 参考文档.py
+++ 参考文档.py
@@ -113,6 +113,10 @@ python使用文档：
         ba_r.append(bal[i])		# list --> bytearray
     b_r = bytes(ba_r)		# bytearray --> bytes
     a_r = int.from_bytes(ba_r, sys.byteorder)		# bytearray --> int
+  42、限流方法：
+    1、基于redis的令牌桶限流：https://blog.csdn.net/u011519550/article/details/109246320
+    2、基于redis的zset实现滑动窗口限流：https://pythonjishu.com/ftberbqixfcngcu/
+    3、计数器、漏桶、令牌桶的三大限流策略：https://blog.csdn.net/crazymakercircle/article/details/130035504
 
 java中的参考文档：
   netty能做什么：https://www.zhihu.com/question/24322387
