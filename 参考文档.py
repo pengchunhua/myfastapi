5a7fe8e0424b200d43083d565539f90f5055eabd
pengchunhua <38714669+pengchunhua@users.noreply.github.com>
2023-12-15 14:04:48 +0800

    Update 参考文档.py

--- 参考文档.py
+++ 参考文档.py
@@ -117,7 +117,9 @@ python使用文档：
     1、基于redis的令牌桶限流：https://blog.csdn.net/u011519550/article/details/109246320
     2、基于redis的zset实现滑动窗口限流：https://pythonjishu.com/ftberbqixfcngcu/
     3、计数器、漏桶、令牌桶的三大限流策略：https://blog.csdn.net/crazymakercircle/article/details/130035504
-  43、python文件下载：https://zhuanlan.zhihu.com/p/139027726
+  43、python文件下载：
+    1、https://zhuanlan.zhihu.com/p/139027726
+    2、https://cloud.tencent.com/developer/article/1471279
 
 java中的参考文档：
   netty能做什么：https://www.zhihu.com/question/24322387
